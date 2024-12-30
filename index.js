2ccfa034654128afc6b62f4b7a4e73947c2b2804
MILI-5 <sanjanawaghmare817@gmail.com>
2024-12-30 13:40:49 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-import { combineReducers } from "redux";
-import searchArticle from "./searchArticle";
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-const reducer = combineReducers({
-  search: searchArticle,
-});
+const root = document.getElementById("root");
+const rootElement = createRoot(root);
 
-export default reducer;
+rootElement.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
